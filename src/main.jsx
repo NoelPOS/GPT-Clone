d8a6e3ff0957ec95c78be33b9579c3fa65287e3a
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-08 23:10:26 +0700

    build structure of project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './routes/homepage/HomePage.jsx'
+import Dashboard from './routes/dashboard/Dashboard.jsx'
+import Chat from './routes/chat/Chat.jsx'
+import Signin from './routes/signin/Signin.jsx'
+import Login from './routes/login/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/dashboard',
+    children: [
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+      {
+        path: '/dashboard/:id',
+        element: <Chat />,
+      },
+    ],
+  },
+  {
+    path: '/signin',
+    element: <Signin />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
