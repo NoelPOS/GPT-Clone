922b24ac26ccf28565012751dc4ad8ffb5b7336e
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-13 12:46:01 +0700

    add type animation

--- src/routes/homepage/HomePage.jsx
+++ src/routes/homepage/HomePage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import orbital from './orbital.png'
 import bot from './bot.png'
+import human1 from './human1.jpeg'
+import human2 from './human2.jpeg'
 import './HomePage.css'
+import { TypeAnimation } from 'react-type-animation'
 
 const HomePage = () => {
+  const [typingStatus, setTypingStatus] = useState('')
   return (
     <div className='homepage'>
       <img src={orbital} className='orbital' />
@@ -24,6 +28,43 @@ const HomePage = () => {
             <div className='bg'></div>
           </div>
           <img src={bot} className='bot' />
+          <div className='chat'>
+            <img
+              src={
+                typingStatus === 'human1'
+                  ? human1
+                  : typingStatus === 'human2'
+                  ? human2
+                  : bot
+              }
+            />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                ' Human1: We produce food for Mice',
+                1000,
+                () => {
+                  setTypingStatus('human1')
+                },
+                'Bot: We produce food for Hamsters',
+                1000,
+                () => {
+                  setTypingStatus('bot')
+                },
+                'Human2: We produce food for Guinea Pigs',
+                1000,
+                () => {
+                  setTypingStatus('human2')
+                },
+              ]}
+              wrapper='span'
+              speed={100}
+              style={{ fontSize: '2em', display: 'inline-block' }}
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
         </div>
       </div>
     </div>
