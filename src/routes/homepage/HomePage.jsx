c9468c3646d084da7e1fbd6a38bfa91c750458f3
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-13 11:34:47 +0700

    add style to homepage

--- src/routes/homepage/HomePage.jsx
+++ src/routes/homepage/HomePage.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import orbital from './orbital.png'
+import bot from './bot.png'
+import './HomePage.css'
 
 const HomePage = () => {
   return (
-    <div>
-      <Link to='/dashboard'>Go to dashboard</Link>
+    <div className='homepage'>
+      <img src={orbital} className='orbital' />
+      <div className='left'>
+        <h1>GPT Clone</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat hic
+          veniam facere maiores aperiam quos repellendus, nostrum magnam.
+          Temporibus, maxime?
+        </h3>
+        <Link to='/dashboard'>Get Started </Link>
+      </div>
+      <div className='right'>
+        <div className='imgcontainer'>
+          <div className='bgcontainer'>
+            <div className='bg'></div>
+          </div>
+          <img src={bot} className='bot' />
+        </div>
+      </div>
     </div>
   )
 }
