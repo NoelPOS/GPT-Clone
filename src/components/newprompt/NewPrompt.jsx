54313f30bdf7ad220bea1bc869d7cca5f28099b9
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-14 15:24:37 +0700

    finalize chat page

---
+++ src/components/newprompt/NewPrompt.jsx
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react'
+import './NewPrompt.css'
+
+const NewPrompt = () => {
+  const endRef = useRef(null)
+  return (
+    <>
+      <div className='endChat' ref={endRef}></div>
+      <form className='newForm'>
+        <label htmlFor='file'>
+          <img src='/attachment.png' alt='' />
+        </label>
+        <input id='file' type='file' multiple={false} hidden />
+        <input type='text' placeholder='Ask anything...' />
+        <button>
+          <img src='/arrow.png' alt='' />
+        </button>
+      </form>
+    </>
+  )
+}
+
+export default NewPrompt
--- src/routes/chat/Chat.jsx
+++ src/routes/chat/Chat.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import './Chat.css'
+import NewPrompt from '../../components/newprompt/NewPrompt'
 
 const Chat = () => {
-  return <div>Chat</div>
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: 'smooth' })
+  })
+
+  const endRef = useRef(null)
+  return (
+    <div className='chatPage'>
+      <div className='wrapper'>
+        <div className='chat'>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message from user</div>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message form</div>
+        </div>
+        <div className='chat'>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message from user</div>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message form</div>
+        </div>
+        <div className='chat'>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message from user</div>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message form</div>
+        </div>
+        <div className='chat'>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message from user</div>
+          <div className='message'>Message from ai</div>
+          <div className='message user'>Message form human</div>
+        </div>
+        <div ref={endRef}></div>
+        <NewPrompt />
+      </div>
+    </div>
+  )
 }
 
 export default Chat
