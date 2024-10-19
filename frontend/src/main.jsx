830ba4340d035f5a68393e80b894816c451eb22e
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-20 02:59:38 +0700

    try hash router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './routes/homepage/HomePage.jsx'
 import Dashboard from './routes/dashboard/Dashboard.jsx'
 import Chat from './routes/chat/Chat.jsx'
@@ -11,41 +11,20 @@ import Login from './routes/login/Login.jsx'
 import RootLayout from './layouts/rootLayout/RootLayout.jsx'
 import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.jsx'
 
-const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/signin',
-        element: <Signin />,
-      },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        element: <DashboardLayout />,
-        children: [
-          {
-            path: '/dashboard',
-            element: <Dashboard />,
-          },
-          {
-            path: '/dashboard/chat/:id',
-            element: <Chat />,
-          },
-        ],
-      },
-    ],
-  },
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/login' element={<Login />} />
+          <Route element={<DashboardLayout />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/dashboard/chat/:id' element={<Chat />} />
+          </Route>
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 )
