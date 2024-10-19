46102ce4f294a46ca6a7bdbee814944e21b7a1be
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-20 02:50:43 +0700

    fix refresh err

--- backend/index.js
+++ backend/index.js
@@ -32,10 +32,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+console.log(path.join(__dirname, '../frontend/dist'))
+
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 })
 
 app.get('/api/upload', (req, res) => {
